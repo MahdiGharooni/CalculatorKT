8df089b9a2237a7a338f75f50b904ea330e9c0cb
M.Gharouni <mmahdi.gjafari69@gmail.com>
2023-10-29 10:04:53 +0330

    imp action & operatin

---
+++ app/src/main/java/ir/mahdi/gharooni/calculatorkt/CalculatorOperation.kt
@@ -0,0 +1,8 @@
+package ir.mahdi.gharooni.calculatorkt
+
+sealed class CalculatorOperation(val symbol: String) {
+    object Add: CalculatorOperation("+")
+    object Subtract: CalculatorOperation("-")
+    object Multiply: CalculatorOperation("x")
+    object Divide: CalculatorOperation("/")
+}
